ac80f7ddf7e186d016a7f91d2d3a3139e168fd54
flannelmonke <khalilanaji@gmail.com>
2024-07-31 23:26:23 -0400

    chore: Refactor code for word frequency analysis and sorting

--- Project.c
+++ Project.c
@@ -6,19 +6,106 @@
 
 int getNextWord(FILE *fp, char *buf, int bufsize);
 
+int CharToInt32(char number[]);
+
+int simplePowerOf(int x, int exp);
+
+void dummyProtocol()
+{
+    printf("It appears that you've tried to use the command line arguments without knowing what it does...\n");
+    printf("This command takes two arguments. One of which is required. And the other being optional.\n");
+    printf("The first argument is the path to the file you would like to read. For example \"a.exe ./path/to/file\"");
+    printf("The second and optional command is the word rankings you would like to display. \nFor example if you would like to see the 5 most common words, you may write \"a.exe ./path/to/file 5\"");
+    printf("NOTE: if you do not use the second argument it will always show the top 10 most commmon words.\n");
+
+    return;
+}
+
 int main(int argc, char *argv[])
 {
-    char text[] = "./alice.txt";
-    FILE *fp;
-    fp = fopen(text, "r");
-    char buffer[32];
 
-    while (getNextWord(fp, buffer, 32))
+    int max = 10;
+    if (argc == 3)
     {
-        printf("%s", buffer);
+        max = atoi(argv[2]);
+    }
+    if (max == 0)
+    {
+        dummyProtocol();
+        return 0;
+    }
+    if (argv[1] == NULL || atoi(argv[1]) != 0)
+    {
+        dummyProtocol();
+        return 0;
+    }
+
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+    char buffer[MAX_WORD];
+
+    WordListPtr AddressOfWords[10];
+
+    // Get the first word and put it in a new node
+    getNextWord(fp, buffer, MAX_WORD);
+    WordListPtr firstWord = createNode(buffer);
+
+    while (getNextWord(fp, buffer, MAX_WORD))
+    {
+
+        WordListPtr temp = firstWord;
+
+        while (temp != NULL)
+        {
+            if ((strcmp(buffer, temp->WordValue.str)) == 0)
+            {
+                // A word already exists in our list
+                temp->WordValue.freq++;
+                break;
+                // Add and escape loop
+            }
+            if (temp->next == NULL)
+            {
+                // End of List and still no match.
+                // Add word to LinkedList
+                temp->next = createNode(buffer);
+                temp = temp->next;
+                break;
+            }
+            temp = temp->next;
+        }
+    }
+
+    fclose(fp);
+
+    WordListPtr temp = merge_sort_bottom_up(firstWord);
+    WordListPtr sorted;
+
+    int i = 0;
+    printf("WORD FREQUENCIES (TOP %d): \n", max);
+    while (temp != NULL)
+    {
+        if (i == max)
+        {
+            break;
+        }
+        printf("%s\t%d\n", temp->WordValue.str, temp->WordValue.freq);
+        temp = temp->next;
+        i++;
     }
 
     return 0;
+}
+
+int simplePowerOf(int x, int exp)
+{
+    int i;
+    int result = x;
+    for (i = 0; i < exp; i++)
+    {
+        result *= x;
+    }
+    return result;
 }
 
 int getNextWord(FILE *fp, char *buf, int bufsize)
--- word.h
+++ word.h
@@ -1,12 +1,140 @@
+#include <stdlib.h>
+#include <string.h>
+
 #ifndef WORD_H__
 #define WORD_H__
 
-#define MAX_WORD 32
+// longest word in the english language is 45 letters, hence the character limit.
+#define MAX_WORD 45
 
 typedef struct word
 {
 	char str[MAX_WORD];
 	int freq;
 } Word;
+
+typedef struct Node
+{
+	Word WordValue;
+	struct Node *next;
+} WordList, *WordListPtr;
+
+WordListPtr createNode(char buffer[])
+{
+	Word data;
+	if (buffer == NULL)
+	{
+	}
+	else
+	{
+		strcpy(data.str, buffer);
+	}
+	data.freq = 1;
+
+	WordListPtr newNode = (WordListPtr)malloc(sizeof(WordList));
+	newNode->WordValue = data;
+	newNode->next = NULL;
+	return newNode;
+}
+
+int get_length(WordListPtr head)
+{
+	int length = 0;
+	while (head)
+	{
+		length++;
+		head = head->next;
+	}
+	return length;
+}
+
+WordListPtr split(WordListPtr head, int size)
+{
+	if (!head)
+	{
+		return NULL;
+	}
+	int i;
+	// ITERATE TO MIDDLE OF LINKED LIST
+	for (i = 0; i < size - 1; i++)
+	{
+		if (head->next != NULL)
+		{
+			head = head->next;
+		}
+		else
+		{
+			break;
+		}
+	}
+	// SAVE ADDRESS OF MIDDLE OF LIST
+	WordListPtr next_head = head->next;
+	// SEVER LINK FROM MIDDLE
+	head->next = NULL;
+	return next_head;
+}
+
+WordListPtr merge(WordListPtr left, WordListPtr right, WordListPtr head)
+{
+	if (!head)
+		return NULL; // Ensure head is not NULL
+
+	WordListPtr current = head;
+	while ((left != NULL) && (right != NULL))
+	{
+		if (left->WordValue.freq > right->WordValue.freq)
+		{
+			current->next = left;
+			left = left->next;
+		}
+		else
+		{
+			current->next = right;
+			right = right->next;
+		}
+		current = current->next;
+	}
+	current->next = left ? left : right;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+
+	return current;
+}
+
+WordListPtr merge_sort_bottom_up(WordListPtr head)
+{
+	if ((head == NULL) || (head->next == NULL))
+	{
+		return head;
+	}
+	int length = get_length(head);
+	int size = 1;
+
+	WordListPtr dummy = createNode(NULL);
+	if (!dummy)
+		return NULL; // Ensure memory allocation was successful
+	dummy->next = head;
+
+	while (size < length)
+	{
+		WordListPtr current = dummy->next;
+		WordListPtr tail = dummy;
+
+		while (current != NULL)
+		{
+			WordListPtr left = current;
+			WordListPtr right = split(left, size);
+			current = split(right, size);
+			tail = merge(left, right, tail); // Corrected order of arguments
+		}
+		size *= 2;
+	}
+
+	WordListPtr sorted_head = dummy->next;
+	free(dummy);
+	return sorted_head;
+}
 
 #endif
